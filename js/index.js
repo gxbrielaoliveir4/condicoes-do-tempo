125fbd25578fded71ecb26b032569208254e3e05
gabriela <gxbolv1@gmail.com>
2024-09-08 15:13:05 -0300

    projeto finalizado

--- js/index.js
+++ js/index.js
@@ -10,16 +10,33 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const form = document.querySelector("#search-form > form");
 const input = document.querySelector("#input-localizacao");
+const sectionTempoInfo = document.querySelector('#tempo-info');
 form === null || form === void 0 ? void 0 : form.addEventListener('submit', (event) => __awaiter(void 0, void 0, void 0, function* () {
     event.preventDefault();
-    if (!input)
+    if (!input || !sectionTempoInfo)
         return;
     const localizaçao = input.value;
     if (localizaçao.length < 3) {
         alert('o local precisa ter pelo menos 3 letras. ');
         return;
     }
-    const resposta = yield fetch(`https://api.openweathermap.org/data/2.5/weather?q=${localizaçao}&appid=f03a51d816916ca067830d65866f9ecc&lang=pt_br&units=metric`);
-    const dados = yield resposta.json();
-    console.log(dados);
+    try {
+        const resposta = yield fetch(`https://api.openweathermap.org/data/2.5/weather?q=${localizaçao}&appid=f03a51d816916ca067830d65866f9ecc&lang=pt_br&units=metric`);
+        const dados = yield resposta.json();
+        const infos = {
+            temperatura: Math.round(dados.main.temp),
+            local: dados.name,
+            icone: `https://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png`
+        };
+        sectionTempoInfo.innerHTML = `
+  <div class="tempo-dados">
+         <h2>${infos.local}</h2>
+
+         <span>${infos.temperatura}</span>
+       </div>
+       <img src="${infos.icone}" />`;
+    }
+    catch (err) {
+        console.log('Deu um erro na obtenção dos dados da API ', err);
+    }
 }));
--- ts/index.ts
+++ ts/index.ts
@@ -1,18 +1,33 @@
 const form = document.querySelector("#search-form > form");
 const input:HTMLInputElement | null = document.querySelector("#input-localizacao");
 
+const sectionTempoInfo = document.querySelector('#tempo-info');
 form?.addEventListener('submit', async (event) => {
   event.preventDefault();
-if(!input ) return
+if(!input || !sectionTempoInfo) return
   const localizaçao = input.value;
 
   if(localizaçao.length < 3 ) {
     alert('o local precisa ter pelo menos 3 letras. ');
     return;
   }
+  try{const resposta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${localizaçao}&appid=f03a51d816916ca067830d65866f9ecc&lang=pt_br&units=metric`)
+  const dados = await resposta.json();
 
-   const resposta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${localizaçao}&appid=f03a51d816916ca067830d65866f9ecc&lang=pt_br&units=metric`)
-   const dados = await resposta.json();
+  const infos = {
+   temperatura:Math.round(dados.main.temp),
+   local:dados.name,
+   icone: `https://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png`
+  };
 
-   console.log(dados);
+  sectionTempoInfo.innerHTML = `
+  <div class="tempo-dados">
+         <h2>${infos.local}</h2>
+
+         <span>${infos.temperatura}</span>
+       </div>
+       <img src="${infos.icone}" />`;
+      }catch(err){
+        console.log('Deu um erro na obtenção dos dados da API ',err);
+      }
 });
